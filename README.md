6d7c49e79cde514b1ff6ef6c30d300c953a1e34a
Parker Moore <parkrmoore@gmail.com>
2019-01-12 19:43:45 -0500

    TypeScript.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# probot-carbon-copy-content
+# probot-typescript
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) that Edit files in one repo, have them seamlessly copied to other repos as pull requests.
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
 
 ## Setup
 
@@ -8,13 +8,16 @@
 # Install dependencies
 npm install
 
+# Run typescript
+npm run build
+
 # Run the bot
 npm start
 ```
 
 ## Contributing
 
-If you have suggestions for how probot-carbon-copy-content could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+If you have suggestions for how probot-typescript could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
 
 For more, check out the [Contributing Guide](CONTRIBUTING.md).
 
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-// Git Data API use case example
-// See: https://developer.github.com/v3/git/ to learn more
-module.exports = app => {
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
+
+export = (app: Application) => {
+  app.on('issues.opened', async (context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    await context.github.issues.createComment(issueComment)
+  })
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+
+  // Git Data API use case example
+  // See: https://developer.github.com/v3/git/ to learn more
   // Opens a PR every time someone installs your app for the first time
-  app.on('installation.created', check)
-  async function check (context) {
+  app.on('installation.created', async (context) => {
     // shows all repos you've installed the app on
     console.log(context.payload.repositories)
 
     const owner = context.payload.installation.account.login
-    context.payload.repositories.forEach(async (repository) => {
+    context.payload.repositories.forEach(async (repository: any) => {
       const repo = repository.name
 
       // Generates a random number to ensure the git reference isn't already taken
@@ -51,10 +62,14 @@ module.exports = app => {
         maintainer_can_modify: true // allows maintainers to edit your app's PR
       })
     })
-  }
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+  app.on('push', async (context) => {
+
+  })
+
 }
